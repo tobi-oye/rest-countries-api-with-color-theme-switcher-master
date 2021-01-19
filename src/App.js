933d79f96408d53a56c34cc8597ba7736b53942f
tobi-oye <tobiloba5000@gmail.com>
2021-01-19 08:23:53 +0100

    [chore] addition of css & scss files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Search from "./Components/Search/Search";
+// import logo from "./logo.svg";
+import "./styles.scss";
+// import Search from "./Components/Search/Search";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Where in the World</h1>
+        <div>Dark Mode</div>
       </header>
-      <section>
-        <Search />
-      </section>
+      <section>{/* <Search /> */}</section>
     </div>
   );
 }
