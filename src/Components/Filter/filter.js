55eae4691943326cf1943858c2decd7f25ad43c8
tobi-oye <tobiloba5000@gmail.com>
2021-01-19 20:17:50 +0100

    [chore] - filter functional component

---
+++ src/Components/Filter/filter.js
@@ -0,0 +1,14 @@
+import "../Filter/filter.scss";
+const Filter = () => {
+  return (
+    <div className="filter-component">
+      <label>Filter by Region</label>
+      <select>
+        <option></option>
+        <option>test 1</option>
+        <option>test 1</option>
+      </select>
+    </div>
+  );
+};
+export default Filter;
