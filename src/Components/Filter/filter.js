08b933b9b2e7cf3c35bfc650de38a6b3a9424c79
tobi-oye <tobiloba5000@gmail.com>
2021-01-22 11:10:14 +0100

    [chore]-Filter Component : Toggling of filter drop down

--- src/Components/Filter/filter.js
+++ src/Components/Filter/filter.js
@@ -1,13 +1,37 @@
+import { useState } from "react/cjs/react.development";
 import "../Filter/filter.scss";
+
 const Filter = () => {
+  const [regionClass, setRegionClass] = useState("region");
+
+  const setRegionClassHandler = () => {
+    if (regionClass === "region") {
+      setRegionClass("region-display");
+    } else {
+      setRegionClass("region");
+    }
+  };
   return (
-    <div className="filter-component">
-      <label>Filter by Region</label>
-      <select>
-        <option></option>
-        <option>test 1</option>
-        <option>test 1</option>
-      </select>
+    <div>
+      <div className="filter-component">
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.15.2/css/all.css"
+          integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu"
+          crossorigin="anonymous"
+        ></link>
+        <label>Filter by Region</label>
+        <i class="fas fa-caret-down" onClick={setRegionClassHandler}></i>
+      </div>
+
+      <div className={regionClass}>
+        <ul>
+          <li>Africa</li>
+          <li>Africa</li>
+          <li>Africa</li>
+          <li>Africa</li>
+        </ul>
+      </div>
     </div>
   );
 };
