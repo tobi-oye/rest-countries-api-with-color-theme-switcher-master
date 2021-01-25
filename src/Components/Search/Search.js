c91e0e2688e96063f9713c7900748af9fddc99f0
tobi-oye <tobiloba5000@gmail.com>
2021-01-25 21:56:09 +0100

    [chore]-addition of style props to search component

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,17 +1,34 @@
 import { useState } from "react";
 import "../Search/Search.scss";
 
-const Search = () => {
+const Search = ({ elemColor, textColor }) => {
   let [prevSearchState, newSearchState] = useState();
+  const styles = {
+    searchComponent: {
+      width: "100%",
+      marginBottom: " 30px",
+    },
+    inputComponent: {
+      width: "100%",
+      height: "35px",
+      backgroundColor: `${elemColor}`,
+      borderRadius: "0.5em",
+      border: "none",
+      padding: "0px 20px",
+      color: `${textColor}`,
+    },
+  };
+  const { searchComponent, inputComponent } = styles;
   return (
-    <div className="search-component">
+    <div className="search-component" style={searchComponent}>
       <input
+        style={inputComponent}
         type="search"
         onChange={(event) => {
           newSearchState(event.target.value.trim());
         }}
         value={prevSearchState}
-        placeholder="  Search for a country"
+        placeholder="Search for a country"
       />
     </div>
   );
